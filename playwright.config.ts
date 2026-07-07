f14d63c726bc629eca71b20b7d1c9f8c7a9941de
Saurav <sauravkmr780@gmail.com>
2026-07-07 03:21:25 -0400

    Allure Reporting

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,9 +14,9 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: [
+reporter: [
     ['line'], 
-    ['html', { open: 'never' }] 
+    ['allure-playwright', { outputFolder: 'allure-results' }]
   ],
   use: {
     baseURL: process.env.BASE_URL,
