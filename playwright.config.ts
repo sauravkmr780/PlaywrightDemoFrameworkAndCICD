8e242f0d75658d4b58c80014f3bee59f7554ad31
Saurav <sauravkmr780@gmail.com>
2026-07-07 03:51:18 -0400

    html Reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,56 +1,49 @@
-import { defineConfig, devices } from '@playwright/test';
-import dotenv from 'dotenv';
-import path from 'path';
+import { defineConfig, devices } from "@playwright/test";
+import dotenv from "dotenv";
+import path from "path";
 //sensitive test data setup
-const ENV = process.env.ENV || 'qa';
+const ENV = process.env.ENV || "qa";
 dotenv.config({ path: path.resolve(__dirname, `.env.${ENV}`) });
 //non -sensitive test data setup
 const testdata = require(`./testdata/${ENV}.json`);
 Object.assign(process.env, testdata);
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-reporter: [
-  ['list'],
-  ['allure-playwright', { 
-    outputFolder: 'allure-results',
-    detail: true,
-    suiteTitle: true
-  }]
-],
+  reporter: [["html", { open: "never" }]],
   use: {
     baseURL: process.env.BASE_URL,
-    trace: 'retain-on-failure-and-retries',
-    video: 'retain-on-failure-and-retries',
-    screenshot: 'only-on-failure',
-    headless: true
+    trace: "retain-on-failure-and-retries",
+    video: "retain-on-failure-and-retries",
+    screenshot: "only-on-failure",
+    headless: true,
   },
 
   projects: [
     // Add a dedicated API testing project that only runs once
     {
-      name: 'api-tests',
+      name: "api-tests",
       testMatch: /.*\.api\.spec\.ts/,
     },
     // Your UI projects should ignore the API test files
     {
-      name: 'chromium',
+      name: "chromium",
       testIgnore: /.*\.api\.spec\.ts/,
-      use: { ...devices['Desktop Chrome'] },
+      use: { ...devices["Desktop Chrome"] },
     },
     {
-      name: 'firefox',
+      name: "firefox",
       testIgnore: /.*\.api\.spec\.ts/,
-      use: { ...devices['Desktop Firefox'] },
+      use: { ...devices["Desktop Firefox"] },
     },
     {
-      name: 'webkit',
+      name: "webkit",
       testIgnore: /.*\.api\.spec\.ts/,
-      use: { ...devices['Desktop Safari'] },
+      use: { ...devices["Desktop Safari"] },
     },
   ],
 });
